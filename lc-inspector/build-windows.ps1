9199a6432a14d1d2b742cf83b29e31bc936abf10
Mateusz Fido <mateusz.fido@org.chem.ethz.ch>
2024-12-18 22:51:28 +0100

    added build files with pyinstaller

---
+++ lc-inspector/build-windows.ps1
@@ -0,0 +1,2 @@
+# WINDOWS:
+pyinstaller --name "LC-Inspector" --icon icon.icns --exclude-module torch --exclude-module matplotlib --exclude-module frozendict --exclude-module torchvision --exclude-module torchgen --exclude-module tqdm --exclude-module torchaudio --add-data "debug.yaml:."  --add-data "ui/logo.png:ui" --add-data "config.json:." --add-data "app.log:." --noconsole --onefile main.py
