e0d46bf6a18769b2663b4b98e2bd533e9a6ebce4
Mateusz Fido <mateusz.fido@org.chem.ethz.ch>
2025-05-09 12:46:29 +0200

    refactor: reshuffled files

--- lc-inspector/build-windows.ps1
+++
@@ -1,2 +0,0 @@
-# WINDOWS:
-pyinstaller --name "LC-Inspector" --icon icon.icns --exclude-module torch --exclude-module matplotlib --exclude-module frozendict --exclude-module torchvision --exclude-module torchgen --exclude-module tqdm --exclude-module torchaudio --add-data "debug.yaml:."  --add-data "ui/logo.png:ui" --add-data "config.json:." --add-data "app.log:." --noconsole --onedir main.py
