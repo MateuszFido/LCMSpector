c975a5bac0a3655637c59867e03308a80dc9c1c9
MateuszFido <mafido@ethz.ch>
2025-05-18 18:40:46 +0200

    test: testing baseline_correction CI

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -2,6 +2,7 @@ import pytest, sys, os
 import numpy as np
 import pandas as pd
 from utils.preprocessing import baseline_correction, construct_xics
+from utils.loading import load_absorbance_data
 from utils.classes import Compound
 
 def test_simple():
@@ -11,10 +12,16 @@ def test_baseline_correction_agilent():
     assert True
 
 def test_baseline_correction_thermo():
-    assert True
+    data = load_absorbance_data('tests/STMIX5mM-pos.txt')
+    baseline_corrected = baseline_correction(data)
+    assert len(baseline_corrected) == len(data)
+    assert baseline_corrected['Value (mAU)'].min() >= 0
 
 def test_baseline_correction_waters():
     assert True
-    
+
 def test_baseline_correction_generic_txt():
-    assert True
+    data = load_absorbance_data('tests/generic.txt')
+    baseline_corrected = baseline_correction(data)
+    assert len(baseline_corrected) == len(data)
+    assert baseline_corrected['Value (mAU)'].min() >= 0
