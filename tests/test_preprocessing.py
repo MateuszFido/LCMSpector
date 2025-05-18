870c7051227aee6284498feb23d0797c2443d33b
MateuszFido <mafido@ethz.ch>
2025-05-18 17:58:00 +0200

    feat: added pytest.ini and improved testing behavior

---
+++ tests/test_preprocessing.py
@@ -0,0 +1,20 @@
+import pytest, sys, os
+import numpy as np
+import pandas as pd
+from utils.preprocessing import baseline_correction, construct_xics
+from utils.classes import Compound
+
+def test_simple():
+    assert True
+
+def test_baseline_correction_agilent():
+    assert True
+
+def test_baseline_correction_thermo():
+    assert True
+
+def test_baseline_correction_waters():
+    assert True
+    
+def test_baseline_correction_generic_txt():
+    assert True
